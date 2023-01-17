26b817b8b666cd92bd8bdc131c192b28648e6251
andryelsa <andrejelsakov0@gmail.com>
2023-01-17 17:31:19 +0300

    init: first version

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const obj = {};
+  let stack = [];
+  bracketsConfig.forEach((char) => (obj[char[1]] = char[0]));
+
+  for (let i = 0; i < str.length; i++) {
+    let el = str[i];
+    if (stack.includes(el) && Object.values(obj).includes(el) && Object.keys(obj).includes(el)) {
+      stack.pop();
+    } else if (Object.values(obj).includes(el)) {
+      stack.push(el);
+    } else if (obj[el] !== stack.pop()) {
+      return false;
+    }
+  }
+
+  return !stack.length;
+};
